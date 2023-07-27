092f48dc1854d5c9d2f05b8e5c1926fecae26018
Sincerely Brittany <sincerelybr@gmail.com>
2023-07-27 16:51:24 -0400

    finishes code

--- FreeCodeCamp/RockPaperScissors/final.py
+++ FreeCodeCamp/RockPaperScissors/final.py
@@ -1,3 +1,50 @@
+# import random 
+
+# def start_game(): 
+#   get_choice = get_choices()
+#   print(get_choice)
+
+# def check_win(player, computer):
+# #   print("You Chose " + player + "Computer Chose " + computer)
+#   print(f"You chose {player} and the computer chose {computer}")
+#   if player == "rock" and computer == "scissors":
+#     print("You win")
+#     return
+#   elif player == "scissors" and computer == "rock":
+#     print("You Lose")
+#     return
+#   elif player == "paper" and computer == "rock":
+#     print("You Win")
+#     return
+#   elif player == "rock" and computer == "paper":
+#     print("You Lose")
+#     return
+#   elif player == "scissors" and computer == "paper":
+#     print("You Win")
+#     return
+#   elif player == "paper" and computer == "scissors":
+#     print("You Lose")
+#     return
+#   elif player == computer:
+#     print("Tie")
+#     return start_game()
+#   else:
+#     print("Try again")
+#     print(player, computer)
+#     return
+
+# def get_choices():
+#   player_choice = input("Enter A Choice: rock, paper, scissors: ")
+
+#   computer_choice = random.choice(["rock", "paper", "scissors"])
+#   choices = {"player": player_choice, "computer": computer_choice}
+
+#   check_win(choices["player"], choices["computer"])
+
+
+# start_game()
+
+
 import random 
 
 def start_game(): 
@@ -43,5 +90,6 @@ def get_choices():
 
 
 start_game()
+
 
   
