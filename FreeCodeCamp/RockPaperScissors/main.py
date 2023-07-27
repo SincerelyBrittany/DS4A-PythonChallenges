3509a82cd89ccc071b2772ff6c6cbace9efb08f1
Sincerely Brittany <sincerelybr@gmail.com>
2023-07-27 15:48:03 -0400

    adds some Rock paper scissor logic

--- FreeCodeCamp/RockPaperScissors/main.py
+++ FreeCodeCamp/RockPaperScissors/main.py
@@ -1,3 +1,7 @@
+import random
+
+
+
 # Variables and Functions
 # Python use underscores when naming variables.
 
@@ -8,34 +12,42 @@
 # indentation is very important for python
 
 def get_choices():
-  player_choice = "rock"
-  computer_choice ="paper"
+  #RPS - User Input
+  player_choice = input("Enter A Choice: rock, paper, scissors: ")
 
+  # RPS - importing libraries, creating list, methods
+  # Python libraries are a set of useful functions so that you dont have to write code from scratch
+  # When you import a code into you program you get access to more features without writing additional code.
+  # We will use the random library to get the computer to choose at random (rock paper scissors)
+
+  # computer_choice ="paper"
+
+  computer_choice = random.choice(["rock", "paper", "scissors"])
+
+  # Dictionaries in python are used to store data data values in key-value pairs 
+  # example: dict = {"name" : "brittany", "color": "red", "choice": choices}
   choices = {"player": player_choice, "computer": computer_choice}
 
-  return player_choice
+  return choices
 
 # Call the get_choices function and store the response in a variable called choices then print it 
 
 get_choice = get_choices()
 print(get_choice)
 
-
-# Dictionaries in python are used to store data data values in key-value pairs 
-# dict = {"name" : "brittany", "color": "red", "choice": choices}
-
-
-
-
+# List are used to store multiple items in a single variable
+# food = ["pizza", "carrots", "eggs"]
+# dinner = random.choice(food)
+# print(dinner)
 
 # RPS - Calling Functions
 
-def greeting():
-  return "Hi"
+# def greeting():
+#   return "Hi"
 
 # Call the function
 
-response = greeting()
-print(response)
+# response = greeting()
+# print(response)
 
 
