33a5a053f237cf711c41a35b71d942fe1ea0b14e
Sincerely Brittany <sincerelybr@gmail.com>
2023-07-29 15:41:00 -0400

    adds some more common features

--- FreeCodeCamp/MostCommonFeatures/index.py
+++ FreeCodeCamp/MostCommonFeatures/index.py
@@ -19,6 +19,14 @@ print(name)
 
 name = "sincerelybritany"; print(name)
 
+
+print("""
+This is a multi-line string
+      
+and you can see the space
+
+""")
+
 # Indentation - you cant randomly indent 
 
 # DataTypes
@@ -127,3 +135,25 @@ def is_adult(age):
     
 def is_adult2(age): 
     return True if age > 18 else False
+
+
+print("SincerlyBrittanyUsingTheUpperMethod".upper())
+print("sincerlyBrittanyUsingTheTitleMethod".title())
+print("sincerlyBrittanyUsingTheTitleMethod".islower())
+
+# isalpha() to check if a string contains only characters and is not empty
+# isalnum() to check if a string contains characters or digits and is not empty
+# isdecimal() to check if a string contains digits and is not empty
+# lower() to get a lowercase version of a string
+# islower() to check if a string is lowercase
+# upper() to get an uppercase version of a string
+# isupper() to check if a string is uppercase
+# title() to get a capitalized version of a string
+# startsswith() to check if the string starts with a specific substring
+# endswith() to check if the string ends with a specific substring
+# replace() to replace a part of a string
+# split() to split a string on a specific character separator
+# strip() to trim the whitespace from a string
+# join() to append new letters to a string
+# find() to find the position of a substring
+
