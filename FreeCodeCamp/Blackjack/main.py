57bcf1fc4e3871e5ea6548979fe245e79b569a7f
Sincerely Brittany <sincerelybr@gmail.com>
2023-07-30 13:10:43 -0400

    sets up class

--- FreeCodeCamp/Blackjack/main.py
+++ FreeCodeCamp/Blackjack/main.py
@@ -1,39 +1,41 @@
 import random
 
-class Deck: 
-    cards =[]
-    suits = ["spades", "hearts", "diamonds", "clubs"]
-    ranks = [
-                    {"rank": "A", "value": 11},
-                    {"rank": "2", "value": 2},
-                    {"rank": "3", "value": 3},
-                    {"rank": "4", "value": 4},
-                    {"rank": "5", "value": 5},
-                    {"rank": "6", "value": 6},
-                    {"rank": "7", "value": 7},
-                    {"rank": "8", "value": 8},
-                    {"rank": "9", "value": 9},
-                    {"rank": "10", "value": 10},
-                    {"rank": "J", "value": 10},
-                    {"rank": "Q", "value": 10},
-                    {"rank": "K", "value": 10},
-                ]
-    for suit in suits:
-        for rank in ranks:
-            cards.append([suit, rank])
-            # print(suit + rank)
-            # cards.append(suit + rank)
+class Deck:
 
-    # print(cards)
+    def __init__(self) -> None:   
+        self.cards =[]
+        suits = ["spades", "hearts", "diamonds", "clubs"]
+        ranks = [
+                        {"rank": "A", "value": 11},
+                        {"rank": "2", "value": 2},
+                        {"rank": "3", "value": 3},
+                        {"rank": "4", "value": 4},
+                        {"rank": "5", "value": 5},
+                        {"rank": "6", "value": 6},
+                        {"rank": "7", "value": 7},
+                        {"rank": "8", "value": 8},
+                        {"rank": "9", "value": 9},
+                        {"rank": "10", "value": 10},
+                        {"rank": "J", "value": 10},
+                        {"rank": "Q", "value": 10},
+                        {"rank": "K", "value": 10},
+                    ]
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append([suit, rank])
+                # print(suit + rank)
+                # cards.append(suit + rank)
 
-    def shuffle():
-        random.shuffle(cards)
+        # print(cards)
+
+    def shuffle(self):
+        random.shuffle(self.cards)
 
 
-    def deal(num):
+    def deal(self, num):
         cards_dealt = []
         for x in range(num):
-            card = cards.pop()
+            card = self.cards.pop()
             cards_dealt.append(card)
         return cards_dealt
     
