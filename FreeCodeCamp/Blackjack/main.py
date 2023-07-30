df3535a84a3b2a565dc3768840eab393261109f9
Sincerely Brittany <sincerelybr@gmail.com>
2023-07-29 22:49:51 -0400

    adds beginning of blackjack logic

--- FreeCodeCamp/Blackjack/main.py
+++ FreeCodeCamp/Blackjack/main.py
@@ -1,4 +1,14 @@
+cards =[]
 suits = ["spades", "hearts", "diamonds", "clubs"]
+ranks = ["A", "2", "3", "4","5","6","7","8","9","10","J","Q","K"]
+for suit in suits:
+    for rank in ranks:
+        # print(suit + rank)
+        cards.append(suit + rank)
+
+print(cards)
+
+
 # suit = suits[1]
 # rank = "K"
 # value = 10
@@ -7,6 +17,5 @@ suits = ["spades", "hearts", "diamonds", "clubs"]
 
 # suits.append("snakes")
 
-for suit in suits:
-    print(suit)
+
 
