3df883df239c2ec651a731d3cbe339171b2c4844
Sincerely Brittany <sincerelybr@gmail.com>
2023-07-29 22:54:39 -0400

    adds blackjack logic

--- FreeCodeCamp/Blackjack/main.py
+++ FreeCodeCamp/Blackjack/main.py
@@ -1,12 +1,23 @@
+import random
+
 cards =[]
 suits = ["spades", "hearts", "diamonds", "clubs"]
 ranks = ["A", "2", "3", "4","5","6","7","8","9","10","J","Q","K"]
 for suit in suits:
     for rank in ranks:
+        cards.append([suit, rank])
         # print(suit + rank)
-        cards.append(suit + rank)
+        # cards.append(suit + rank)
 
-print(cards)
+# print(cards)
+
+random.shuffle(cards)
+# print(cards)
+
+card = cards.pop()
+print(card)
+
+
 
 
 # suit = suits[1]
